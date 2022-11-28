3c083111f6dfa0fa64feea99e6a7358a0d66b677
Rafael Figliuolo Nascimento <rafaelfnascimento1230@gmail.com>
2022-11-28 00:09:20 -0300

    .

--- Beginner/1188 - Inferior Area.py
+++ Beginner/1188 - Inferior Area.py
@@ -17,11 +17,11 @@ Print the calculated result (sum or average), with one digit after the decimal p
 
 letra=input()
 resp=0
-m=[[1,2,3,4,5,6,7],[8,9,10,11,12,13,14],[15,16,17,18,19,20,21],[22,23,24,25,26,27,28],[29,30,31,32,33,34,35],[36,37,38,39,40,41,42],[43,44,45,46,47,48,49]]
-'''for l in range (12):
+m=[[],[],[],[],[],[],[],[],[],[],[],[]]
+for l in range (12):
     for c in range (12):
         num=float(input())
-        m[l].append(num)'''
+        m[l].append(num)
 
 cont=0
 media=0
@@ -29,22 +29,15 @@ index=len(m)-1
 linha=len(m)
 minimo=linha//2
 maximo=linha//2
-print(index)
 if (index//2 != index/2):
     linha+=1
     minimo-=1
 
 for x in m[linha//2+1:]:   
     for y in x:
-        print("Y: ",y)
         if cont>=minimo and cont<=maximo:
             resp+=y
             media+=1 
-        print("MAX: ",maximo)
-        print("MIN: ",minimo)
-        print("RESP: ",resp)
-        print("CONT: ",cont)
-        print("INDEX: ",index) 
         cont+=1
     index-=1
     minimo-=1
---
+++ Beginner/1189 - Left Area.py
@@ -0,0 +1,63 @@
+'''
+Read an uppercase character that indicates an operation that will be performed in an array M[12][12]. Then, calculate and print the sum or average considering only that numbers that are included in the green area of this array, like shown in the following figure.
+
+
+
+
+Input
+The first line of the input contains a single uppercase character O ('S' or 'M'), indicating the operation Sum or Average (Média in portuguese) to be performed with the elements of the array. Follow 144 floating-point numbers of the array.
+
+Output
+Print the calculated result (sum or average), with one digit after the decimal point.
+
+Input Sample	Output Sample
+S
+5.0
+130.0
+-3.5
+2.5
+4.1
+...
+
+122.2
+'''
+
+letra=input()
+resp=0
+m=[[],[],[],[],[],[],[],[],[],[],[],[]]
+for l in range (12):
+    for c in range (12):
+        num=float(input())
+        m[l].append(num)
+
+cont_coluna=0
+cont_linha=0
+cont=0
+media=0
+index=0
+linha=0
+coluna=0
+tamanho_de_m=len(m)
+if len(m)/2==len(m)//2:
+    tamanho_de_m-=1
+for x in m:   
+    for y in x:
+        if cont_linha>0 and cont_linha<len(m)-1:
+            if cont_coluna<=coluna:
+                resp+=y
+                media+=1    
+        cont_coluna+=1 
+
+    if cont_linha>0 and cont_linha<len(m)-1:
+        if len(m)/2==len(m)//2 and index==(tamanho_de_m)//2:
+            coluna+=1
+        if index<tamanho_de_m//2:
+            coluna+=1
+        else:
+            coluna-=1   
+    index+=1
+    cont_linha+=1
+    cont_coluna=0
+if letra=="M":
+    resp/=media
+print("%.1f"%resp)
