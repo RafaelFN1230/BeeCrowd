881167d1204da67732f4022b3cbf69b735b346de
Rafael Figliuolo Nascimento <rafaelfnascimento1230@gmail.com>
2023-12-18 11:50:33 -0300

    Adição da questão e resolução da atividade 1165

---
+++ Beginner/1165 - Prime Number.py
@@ -0,0 +1,47 @@
+'''A Prime Number is a number that is divisible only by 1 (one) and by itself. 
+For example the number 7 is Prime, because it can be divided only by 1 and by 7.
+
+Input
+The input contains several test cases. The first contains the number of test cases N (1 ≤ N ≤ 100). 
+Each one of the following N lines contains an integer X (1 < X ≤ 107), that can be or not a prime number.
+
+Output
+For each test case print the message “X eh primo” (X is prime) or “X nao eh primo” (X isn't prime) 
+according with to above specification.
+
+Input Sample	
+3
+8
+51
+7
+
+Output Sample
+
+8 nao eh primo
+51 nao eh primo
+7 eh primo
+'''
+def valor_primo(valor):
+    divisor = 1
+    n_divisores = 0
+    while divisor <= valor:
+        resto = valor%divisor
+
+        if resto == 0:
+            n_divisores +=1
+        
+        divisor +=1
+    
+    if n_divisores ==2:
+        return True
+    else:
+        return False
+
+numero_testes = int(input())
+
+for teste in range(numero_testes):
+    valor = int(input())
+    if valor_primo(valor):
+        print(valor," eh primo")
+    else:
+        print(valor," nao eh primo")
